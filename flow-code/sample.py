8008d01300a9e0ef63fe3144ed6185ba7fe8c1f5
Nathan Simpson <phinate@protonmail.com>
2022-08-09 20:58:31 +0200

    more code

--- flow-code/sample.py
+++ flow-code/sample.py
@@ -17,28 +17,6 @@ def _sample_flow(flow, feature_scaler, context_scaler=None, context=None, num_sa
 def hist2d_from_flows_with_error(
     flow_list,  x_bins, y_bins, feature_scaler, context_scaler=None, context=None, num_samples=10000, density=False
 ):
-    """
-    Args:
-        truth_masses: array-like, should have shape [num_signal_points, 2], e.g. [[500, 250], [900, 100]]
-        num_samples: int, number of samples to generate per signal point
-        path_to_flows: str, path to a folder containing 1 or more trained flows with .pt file extensions.
-                       can also be a state_dict, or a list of state_dicts.
-        scalers: tuple(StandardScaler, StandardScaler), set of scalers for reco and truth data.
-        mjj_bin_edges: array-like, 1-D array of bin edges in the mjj axis
-        mbbyy_bin_edges: array-like, 1-D array of bin edges in the mbbyy axis
-        flow_maker: callable that returns a Flow object. needs to match the architecture of the .pt files.
-    Returns:
-        tuple(array, array): (average of histograms over all flows (shape: [len(truth_masses), num_samples, 2]),
-                              standard deviation (shape: [len(truth_masses), num_samples, 2]))
-    Example usage:
-        # define binning, get scalers
-        mjj_range = np.linspace(0, 600, 100)
-        mbbyy_range = np.linspace(200, 1500, 100)
-        scalers = load_scalers("trained_flows")
-        # generate histograms from 10k samples per flow at mX, mS = [[1000,500], [1600,200]]
-        # flows and scalers are in the folder "trained_flows"
-        hists, errors = hists_from_flows_with_errors([[1000,500], [1600,200]], 10000, "trained_flows", scalers, mjj_range, mbbyy_range)
-    """
     flow_samples = np.array([_sample_flow(flow, feature_scaler, context_scaler, context, num_samples) for flow in flow_list])
 
     if (len(np.array(x_bins).shape)) > 1:
